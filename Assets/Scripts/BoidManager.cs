6b7d9e6050d34fcddb3f54c5e3f7e8b548ab1708
Aryan Nag <147342659+AryanNag29@users.noreply.github.com>
2026-05-18 12:42:01 +0530

    Init Push

---
+++ Assets/Scripts/BoidManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour
+{
+    #region Variables
+
+    const int threadGroupSize = 1024;
+
+    #endregion
+
+    #region References
+
+    public BoidSettings settings;
+    public ComputeShader compute;
+    Boid[] boids;
+
+    #endregion
+
+
+    #region Start/Update
+
+    void Start()
+    {
+        boids = FindObjectsOfType<Boid>();
+        foreach (Boid b in boids)
+        {
+            b.Initialize(settings, null);
+        }
+    }
+
+
+    void Update()
+    {
+        if (boids != null)
+        {
+            int numBoids = boids.Length;
+            var boidData = new BoidData[numBoids];
+
+            for (int i = 0; i < boids.Length; i++)
+            {
+                boidData[i].position = boids[i].position;
+                boidData[i].direction = boids[i].forward;
+            }
+
+            var boidBuffer = new ComputeBuffer(numBoids, BoidData.Size);
+            boidBuffer.SetData(boidData);
+
+            compute.SetBuffer(0, "boids", boidBuffer);
+            compute.SetInt("numBoids", boids.Length);
+            compute.SetFloat("viewRadius", settings.perceptionRadius);
+            compute.SetFloat("avoidRadius", settings.avoidanceRadius);
+
+            int threadGroups = Mathf.CeilToInt(numBoids / (float)threadGroupSize);
+            compute.Dispatch(0, threadGroups, 1, 1);
+
+            boidBuffer.GetData(boidData);
+
+            for (int i = 0; i < boids.Length; i++)
+            {
+                boids[i].avgFlockHeading = boidData[i].flockHeading;
+                boids[i].centreOfFlockmates = boidData[i].flockCentre;
+                boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
+                boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
+
+                boids[i].UpdateBoid();
+            }
+
+            boidBuffer.Release();
+        }
+    }
+
+    #endregion
+
+    #region Structure
+
+    public struct BoidData
+    {
+        public Vector3 position;
+        public Vector3 direction;
+
+        public Vector3 flockHeading;
+        public Vector3 flockCentre;
+        public Vector3 avoidanceHeading;
+        public int numFlockmates;
+
+        public static int Size
+        {
+            get { return sizeof(float) * 3 * 5 + sizeof(int); }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    #region Enum
+
+    public enum GizmoType
+    {
+        Never,
+        SelectedOnly,
+        Always
+    }
+
+    #endregion
+
+    #region Reference
+
+    public Boid prefab;
+
+    #endregion
+
+    #region Variables
+
+    public float spawnRadius = 10;
+    public int spawnCount = 10;
+    public Color colour;
+    public GizmoType showSpawnRegion;
+
+    #endregion
+
+    #region Awake
+
+    void Awake()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 pos = transform.position + Random.insideUnitSphere * spawnRadius;
+            Boid boid = Instantiate(prefab);
+            boid.transform.position = pos;
+            boid.transform.forward = Random.insideUnitSphere;
+
+            boid.SetColour(colour);
+        }
+    }
+
+    #endregion
+
+    #region Gizmos
+
+    private void OnDrawGizmos()
+    {
+        if (showSpawnRegion == GizmoType.Always)
+        {
+            DrawGizmos();
+        }
+    }
+
+    #endregion
+
+    #region Functions
+
+    void OnDrawGizmosSelected()
+    {
+        if (showSpawnRegion == GizmoType.SelectedOnly)
+        {
+            DrawGizmos();
+        }
+    }
+
+    void DrawGizmos()
+    {
+        Gizmos.color = new Color(colour.r, colour.g, colour.b, 0.3f);
+        Gizmos.DrawSphere(transform.position, spawnRadius);
+    }
+
+    #endregion
+}
